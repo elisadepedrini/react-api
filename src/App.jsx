ffda6aa47a57796000890089dab46ddd0ef20d8f
elisadepedrini <elisa.depedrini@gmail.com>
2026-05-08 10:48:20 +0200

    Print in console actors's list with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react"
 
 
 function App() {
  
+  const api_url = "https://lanciweb.github.io/demo/api/actresses/"
+  
+
+  useEffect(() => { 
+    fetch(api_url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+        
+    })
+  })
 
   return (
     <>
-      
+
     </>
   )
 }
